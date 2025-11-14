330f749bda5c0e2db7aead93f22bc5a2a7a387dd
earlEstandarte <earljohn.estandarte@foundationu.com>
2025-11-14 16:29:49 +0800

    add: variables hands on

---
+++ python-basics/variables.py
@@ -0,0 +1,14 @@
+# this is a python comment
+
+# multi
+# line
+# python comment
+
+#variables and dynamic typing
+my_var=10
+name="Earl"
+character='a'
+
+#this is the best practice for improved readability, this is called "TYPE HINTING"
+name: int = "Earl John"
+age: int = 21
