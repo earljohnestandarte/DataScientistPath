14043115ad005da60426e9aa0ea9bd2a940a76a5
earlEstandarte <earljohn.estandarte@foundationu.com>
2025-11-14 16:48:04 +0800

    add: core data types hands on

---
+++ python-basics/core-data-types.py
@@ -0,0 +1,30 @@
+# Numbers: int (arbitrary-precision integers), float (64-bit)
+#
+# Boolean: True and False (note the capital 'T' and 'F')
+#
+# The "Null" Type: None (also capitalized)
+#
+# Strings: str. They are immutable.
+
+# Type Hinting
+num: int = 10
+decimal: float = 3.14
+word: str = "Hello"
+boolean: bool = True
+null: None = None
+
+# Outputting Types
+print(type(num))
+print(type(decimal))
+print(type(word))
+print(type(boolean))
+print(type(null))
+
+# Outputting values
+print(num)
+print(decimal)
+print(word)
+print(boolean)
+print(null)
+
+
